2c79f9fea0191484a8661d93c3d8c1225b0e4e24
zanewebbUW <webbz@uw.edu>
2019-03-09 17:59:29 -0800

    Moved some files around

--- servers/messaging/models/messageModel.js
+++ servers/messaging/models/messageModel.js
@@ -3,7 +3,7 @@ const AutoIncrement = require('mongoose-sequence')(mongoose);
 const Schema = mongoose.Schema;
 
 let messageSchema = new Schema({
-    channelID: {type: Number, required: true},
+    streamID: {type: Number, required: true},
     body: {type: String, required: true},
     createdAt: {type: Date, required: true, default: Date.now},
     creator: {type: Number, required: true},
